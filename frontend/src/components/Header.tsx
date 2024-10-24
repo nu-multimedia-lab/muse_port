284365699db715f08eb0ae904bfde2e0e3de98fa
tmba <ut42.nu@gmail.com>
2024-10-24 09:22:59 +0900

    🚸 ヘッダーとモード切り替えボタンの修正

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,8 +4,9 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="fixed w-full h-24 flex justify-between items-center px-16 bg-neutral-300 dark:bg-neutral-800">
-      <Link href="/" className="font-bold text-xl">
+    <header className="fixed w-full h-24 z-50 flex justify-between items-center px-16 bg-neutral-300 dark:bg-neutral-800">
+      <Link href="/" className="font-bold text-xl flex gap-2 items-center">
+        <div className="w-8 h-8 rounded-full bg-blue-500" />
         MUSE PORT
       </Link>
       <div className="flex items-center gap-4">
