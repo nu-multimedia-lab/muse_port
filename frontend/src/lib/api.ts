350684054c9db0dfe9c3dc4d33a84f2da11f72f9
tmba <ut42.nu@gmail.com>
2024-11-01 16:03:25 +0900

    ➕ Article型を追加し、記事データをFastAPIからフェッチするfetchArticles関数を実装

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -7,6 +7,12 @@ export type Post = {
   body: string;
 };
 
+export type Article = {
+  id: number;
+  title: string;
+  content: string;
+};
+
 export const fetchPosts = async (): Promise<Post[]> => {
   const res: Post[] = await axios
     .get("https://jsonplaceholder.typicode.com/posts")
@@ -17,6 +23,13 @@ export const fetchPosts = async (): Promise<Post[]> => {
 export const fetchPostById = async (id: number): Promise<Post> => {
   const res: Post = await axios
     .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((res) => res.data);
+  return res;
+};
+
+export const fetchArticles = async (): Promise<Article[]> => {
+  const res: Article[] = await axios
+    .get("http://localhost:8000/articles")
     .then((res) => res.data);
   return res;
 };
