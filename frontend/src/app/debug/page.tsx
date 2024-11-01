98329a89cbfb97cb0313ea052faa67fb836eac24
tmba <ut42.nu@gmail.com>
2024-11-01 16:04:26 +0900

    🚸  フォールバックを用いて記事を表示するArticlesコンポーネントを追加し、fetchArticles関数を用いて記事データを取得

--- frontend/src/app/debug/page.tsx
+++ frontend/src/app/debug/page.tsx
@@ -1,11 +1,43 @@
 import { PostCard } from "@/components/PostCard";
 import { PostCardSkeleton } from "@/components/skeleton/PostCardSkeleton";
-import { fetchPosts, Post } from "@/lib/api";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Article, fetchArticles, fetchPosts, Post } from "@/lib/api";
 import React, { Suspense } from "react";
 
-const Debug = async () => {
+const Articles = async () => {
+  const articles: Article[] = await fetchArticles();
+
+  return (
+    <ul>
+      {articles.map((article) => (
+        <li key={article.id}>
+          <h2>{article.title}</h2>
+          <p>{article.content}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const Posts = async () => {
   const posts: Post[] = await fetchPosts();
 
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {posts.map((post: Post) => (
+        <PostCard
+          key={post.id}
+          id={post.id}
+          userId={post.userId}
+          title={post.title}
+          body={post.body}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Debug = async () => {
   return (
     <main>
       <div className="h-screen grid place-items-center bg-neutral-50 dark:bg-neutral-900">
@@ -17,19 +49,17 @@ const Debug = async () => {
           </p>
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-8 p-32 bg-zinc-100 dark:bg-neutral-800">
-        <h1 className="col-span-3 text-4xl font-extrabold">Dummy Posts</h1>
-        {posts.map((post) => (
-          <Suspense fallback={<PostCardSkeleton />}>
-            <PostCard
-              key={post.id}
-              id={post.id}
-              userId={post.userId}
-              title={post.title}
-              body={post.body}
-            />
-          </Suspense>
-        ))}
+      <div className="flex flex-col items-center gap-4 p-16">
+        <h1 className="text-4xl font-extrabold">Articles</h1>
+        <Suspense fallback={<Skeleton className="w-64 h-16 bg-amber-400" />}>
+          <Articles />
+        </Suspense>
+      </div>
+      <div className="grid gap-8 p-16 bg-zinc-100 dark:bg-neutral-800">
+        <h1 className="text-4xl font-extrabold">Dummy Posts</h1>
+        <Suspense fallback={<PostCardSkeleton />}>
+          <Posts />
+        </Suspense>
       </div>
     </main>
   );
