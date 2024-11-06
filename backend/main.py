572649f55c6e1c8d42fb8467c5f9493f49b907f3
tmba <ut42.nu@gmail.com>
2024-11-07 01:59:26 +0900

    ✨ 新しいエンドポイント'/items/{item_id}'を追加し、アイテム情報を取得できるようにしました

--- backend/main.py
+++ backend/main.py
@@ -18,4 +18,9 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+
 handler = Mangum(app)
