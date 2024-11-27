bd21892f5944fcff460758848460ab02e9260025
tmba <ut42.nu@gmail.com>
2024-11-28 02:48:06 +0900

    ✨ ユーザーAPIの機能を追加し、axiosインスタンスを使用してCRUD操作を実装

--- frontend/src/lib/apis/user.ts
+++ frontend/src/lib/apis/user.ts
@@ -1,12 +1,33 @@
 import { User } from "@/lib/types";
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
-const UserApi = axios.create({
+const apiClient: AxiosInstance = axios.create({
   baseURL: `${process.env.BACKEND_API_URL}/users`,
 });
 
-export const fetchUsers = async (): Promise<User[]> => {
-  const res: User[] = await UserApi.get("/").then((res) => res.data);
-
+const getAllUsers = async (): Promise<User[]> => {
+  const res: User[] = await apiClient.get("/").then((res) => res.data);
   return res;
 };
+
+const getUser = async (id: string): Promise<User> => {
+  const res: User = await apiClient.get(`/${id}`).then((res) => res.data);
+  return res;
+};
+
+const createUser = async (data: User): Promise<User> => {
+  const res: User = await apiClient.post("/", data).then((res) => res.data);
+  return res;
+};
+
+const updateUser = async (id: string, data: User): Promise<User> => {
+  const res: User = await apiClient.put(`/${id}`, data).then((res) => res.data);
+  return res;
+};
+
+const deleteUser = async (id: string): Promise<User> => {
+  const res: User = await apiClient.delete(`/${id}`).then((res) => res.data);
+  return res;
+};
+
+export { getAllUsers, getUser, createUser, updateUser, deleteUser };
