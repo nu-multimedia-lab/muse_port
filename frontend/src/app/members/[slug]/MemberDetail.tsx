492e18db088e05aae3d1cc08832b72e52e97e6c3
tmba <ut42.nu@gmail.com>
2024-12-03 22:45:44 +0900

    ♻️ UserCardコンポーネントをMemberDetailにリネームし、ユーザー詳細表示を改善

--- frontend/src/app/members/[slug]/MemberDetail.tsx
+++ frontend/src/app/members/[slug]/MemberDetail.tsx
@@ -1,3 +1,4 @@
+import { UserCard } from "@/components/elements/UserCard";
 import { getUser } from "@/lib/apis/user";
 import { User } from "@/lib/types";
 import { notFound } from "next/navigation";
@@ -8,15 +9,17 @@ type UserProps = {
   userId: string;
 };
 
-export const UserCard = async (props: UserProps) => {
+export const MemberDetail = async (props: UserProps) => {
   try {
     const user: User = await getUser(props.userId);
+
     return (
-      <ul className="m-4 p-4 bg-zinc-200 rounded-md text-black">
-        <p>ID: {user.id}</p>
-        <p>アカウント名: {user.username}</p>
-        <p>概要: {user.bio === null ? "記載がありません" : user.bio}</p>
-      </ul>
+      <UserCard
+        imgSrc="null"
+        userId={user.id}
+        userName={user.username}
+        bio={user.bio}
+      />
     );
   } catch (error) {
     notFound();
--- frontend/src/app/members/[slug]/page.tsx
+++ frontend/src/app/members/[slug]/page.tsx
@@ -1,16 +1,20 @@
 import Loading from "@/app/members/[slug]/loading";
-import { UserCard } from "@/app/members/[slug]/UserCard";
+import { MemberDetail } from "@/app/members/[slug]/MemberDetail";
 import React, { Suspense } from "react";
 
 const MemberPage = ({ params }: { params: { slug: string } }) => {
   const userId: string = params.slug;
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-extrabold mt-24">Member Detail</h1>
-      <Suspense fallback={<Loading />}>
-        <UserCard userId={userId} />
-      </Suspense>
+    <div className="min-h-screen">
+      <div className="flex flex-col gap-8">
+        <h1 className="text-4xl font-extrabold mt-32 mx-auto">Member Detail</h1>
+        <div className="mx-auto min-w-[720px]">
+          <Suspense fallback={<Loading />}>
+            <MemberDetail userId={userId} />
+          </Suspense>
+        </div>
+      </div>
     </div>
   );
 };
